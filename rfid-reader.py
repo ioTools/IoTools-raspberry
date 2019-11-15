34c0c9e5477e08d91c07fd53cbe5438d0db64e81
asdf1899 <araid.anas99@gmail.com>
2019-11-16 00:11:28 +0100

    Add send get request

--- rfid-reader.py
+++ rfid-reader.py
@@ -4,15 +4,47 @@ import RPi.GPIO as GPIO
 import sys
 sys.path.append('/home/pi/MFRC522-python')
 from mfrc522 import SimpleMFRC522
+import gps
+import requests
+
+#=========================================
+# CONTIENE L'ID DEL FURGONE (GIA INSERITO NEL DB)
+try
+    fh=open("idFurgone.txt","r")
+    idFurgone = fh.read()
+except
+    idFurgone= None
+#=========================================
+
 
 reader = SimpleMFRC522()
 
 print("Avvicina il tag rfid")
 
 try:
-    id, timestamps = reader.read()
-    print(id)
-    print(text)
+    # legge ID e Timestamps dal tag
+    data = reader.read()
+    data.split("_")
+    idAttrezzo = data[0]
+    time= data[1]
+    
+    # avvia una sessione per il gps
+    session = gps.gps("localhost", "2947")
+    session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+    while True :
+        rep = session.next()
+        try :
+            if (rep["class"] == "TPV") :
+             		print(str(rep.lat) + "," + str(rep.lon))
+                    lat = str(rep.lat)
+                    long = str(rep.lon)
+                    GETdata = {"idFurgone": idFurgone, "idAttrezzo": idAttrezzo, "timestamp": time, "lat": lat, "long": long}
+                    r = requests.get("http://www.iotools.altervista.com/api/getRfidData.php", params=GETdata)
+                    # http://www.iotools.altervista.com/api/getRfidData.php?idFurgone=1&ecc...                   
+                    print(r.url)
+                    break
+        except Exception as e :
+            print("Got exception " + str(e))
 
 finally:
     GPIO.cleanup()
